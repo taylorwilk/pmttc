b24e61eecad4918862afbb2c451c84ec9f8de313
Taylor Wilk <twilk@atpco.net>
2024-04-05 11:31:07 -0400

    update routing error

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -12,9 +12,7 @@ export class LandingPageComponent implements OnInit{
   constructor(private route: ActivatedRoute) {}
 
   ngOnInit(): void {
-    let path = this.route.snapshot.url[0].path
+    let path = this.route.snapshot.url[0]?.path
     document.getElementById(path)?.scrollIntoView()
-   
-      
   }
 }
